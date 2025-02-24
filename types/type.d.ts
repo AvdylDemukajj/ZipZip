742d36a44de455bbda0469d61e27497a33ed672d
AvdylDemukajj <avdyldemukaj38@gmail.com>
2025-02-24 16:27:01 +0100

    "interface for Button props for touchable opacity"

--- types/type.d.ts
+++ types/type.d.ts
@@ -53,3 +53,12 @@ declare interface Ride {
     };
 }
 
+declare interface ButtonProps extends TouchableOpacityProps {
+    title: string;
+    bgVariant?: "primary" | "secondary" | "danger" | "outline" | "success";
+    textVariant?: "primary" | "default" | "secondary" | "danger" | "success";
+    IconLeft?: React.ComponentType<any>;
+    IconRight?: React.ComponentType<any>;
+    className?: string;
+}
+
