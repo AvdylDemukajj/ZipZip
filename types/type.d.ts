65a10e2734e68ebcfbd111bfd1d7687b0cf382af
AvdylDemukajj <avdyldemukaj38@gmail.com>
2025-02-24 16:27:27 +0100

    "iterface for Google input props"

--- types/type.d.ts
+++ types/type.d.ts
@@ -62,3 +62,19 @@ declare interface ButtonProps extends TouchableOpacityProps {
     className?: string;
 }
 
+declare interface GoogleInputProps {
+    icon?: string;
+    initialLocation?: string;
+    containerStyle?: string;
+    textInputBackgroundColor?: string;
+    handlePress: ({
+                      latitude,
+                      longitude,
+                      address,
+                  }: {
+        latitude: number;
+        longitude: number;
+        address: string;
+    }) => void;
+}
+
