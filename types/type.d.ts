c35634d44c15778eb6ae26550a8bd960cfbe5983
AvdylDemukajj <avdyldemukaj38@gmail.com>
2025-02-24 16:24:36 +0100

    "interface for driver  "

---
+++ types/type.d.ts
@@ -0,0 +1,12 @@
+import {TextInputProps, TouchableOpacityProps} from "react-native";
+
+declare interface Driver {
+    driver_id: number;
+    first_name: string;
+    last_name: string;
+    profile_image_url: string;
+    car_image_url: string;
+    car_seats: number;
+    rating: number;
+}
+
