35159c3647aa8f54c6712d1c1d87beaf1bd58ea2
AvdylDemukajj <avdyldemukaj38@gmail.com>
2025-02-24 16:28:35 +0100

    "interface for location store"

--- types/type.d.ts
+++ types/type.d.ts
@@ -97,3 +97,30 @@ declare interface PaymentProps {
     rideTime: number;
 }
 
+declare interface LocationStore {
+    userLatitude: number | null;
+    userLongitude: number | null;
+    userAddress: string | null;
+    destinationLatitude: number | null;
+    destinationLongitude: number | null;
+    destinationAddress: string | null;
+    setUserLocation: ({
+                          latitude,
+                          longitude,
+                          address,
+                      }: {
+        latitude: number;
+        longitude: number;
+        address: string;
+    }) => void;
+    setDestinationLocation: ({
+                                 latitude,
+                                 longitude,
+                                 address,
+                             }: {
+        latitude: number;
+        longitude: number;
+        address: string;
+    }) => void;
+}
+
