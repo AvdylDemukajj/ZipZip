65aca3d2dcbfe5d9855f0c7284dd3d90bcb20847
AvdylDemukajj <avdyldemukaj38@gmail.com>
2025-02-25 12:50:58 +0100

    "oauth component style"

---
+++ components/OAuth.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+
+const OAuth = () => (
+     <View>
+        <View className="flex flex-row justify-center mt-4 gap-x-3 items-center">
+            <View className="flex-1 h-[1px] bg-general-100"/>
+            <Text className="text-lg">Or </Text>
+            <View className="flex-1 h-[1px] bg-general-100"/>
+        </View>
+     </View>
+)
