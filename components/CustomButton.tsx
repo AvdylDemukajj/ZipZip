92b1df42a6fbd1709066286447505651dbfc19e2
AvdylDemukajj <avdyldemukaj38@gmail.com>
2025-02-25 10:06:35 +0100

    "customizing buttons style"

--- components/CustomButton.tsx
+++ components/CustomButton.tsx
@@ -1,4 +1,22 @@
 import { Text, TouchableOpacity } from 'react-native'
+import { ButtonProps } from "@/types/type"
+
+
+const getTextVariantStyle = (variant: ButtonProps['textVariant']) => {
+    switch(variant){
+        case "primary":
+            return "bg-black"
+        case "secondary":
+            return "bg-gray-100"
+        case "success":
+            return "bg-green-100"
+        case "danger":
+            return "bg-red-100"
+        default:
+            return "bg-white "
+    }
+ 
+}
 
 export const CustomButton = ({
     onPress, 
@@ -8,9 +26,9 @@ export const CustomButton = ({
     IconLeft, 
     IconRight, 
     className, 
-    ...props}) => <TouchableOpacity onPress={onPress}>
+    ...props}: ButtonProps) => <TouchableOpacity onPress={onPress} className={`w-full rounded-full flex flex-row justify-center items-center shadow-md shadow-neutral-400/70 ${getBgVariantStyle(bgVariant)} ${className}` }>
         {IconLeft && <IconLeft />}
-        <Text></Text>
+       
         {IconRight && <IconRight />}
     </TouchableOpacity>
   
