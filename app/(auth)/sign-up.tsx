ae2b3b02dab462552902b09b063cb3b74332fade
AvdylDemukajj <avdyldemukaj38@gmail.com>
2025-03-27 11:18:17 +0100

    "adding clerk to the signup page"

--- app/(auth)/sign-up.tsx
+++ app/(auth)/sign-up.tsx
@@ -1,73 +1,116 @@
-import { CustomButton } from "@/components/CustomButton";
+import { useSignUp } from "@clerk/clerk-expo";
+import { Link, router } from "expo-router";
+import { useState } from "react";
+import { Alert, Image, ScrollView, Text, View } from "react-native";
+
 import InputField from "@/components/InputField";
 import OAuth from "@/components/OAuth";
 import { icons, images } from "@/constants";
-import { Link } from "expo-router";
-import { useState } from "react";
-import { Image, ScrollView, Text, View } from "react-native"
 
- const SignUp = () => {
+const SignUp = () => {
+  const { isLoaded, signUp, setActive } = useSignUp();
+  const [showSuccessModal, setShowSuccessModal] = useState(false);
 
-    const onSignUpPress = async() =>{
-        // TODO: Call the API to sign up the user
-        console.log("SignUp pressed", form);
+  const [form, setForm] = useState({
+    name: "",
+    email: "",
+    password: "",
+  });
+  const [verification, setVerification] = useState({
+    state: "default",
+    error: "",
+    code: "",
+  });
+
+  const onSignUpPress = async () => {
+    if (!isLoaded) return;
+    try {
+      await signUp.create({
+        emailAddress: form.email,
+        password: form.password,
+      });
+      await signUp.prepareEmailAddressVerification({ strategy: "email_code" });
+      setVerification({
+        ...verification,
+        state: "pending",
+      });
+    } catch (err: any) {
+      console.log(JSON.stringify(err, null, 2));
+      Alert.alert("Error", err.errors[0].longMessage);
     }
+  };
+  const onPressVerify = async () => {
+   
+  };
+  return (
+    <ScrollView className="flex-1 bg-white">
+      <View className="flex-1 bg-white">
+        <View className="relative w-full h-[250px]">
+          <Image source={images.signUpCar} className="z-0 w-full h-[250px]" />
+          <Text className="text-2xl text-black font-JakartaSemiBold absolute bottom-5 left-5">
+            Create Your Account
+          </Text>
+        </View>
+        <View className="p-5">
+          <InputField
+            label="Name"
+            placeholder="Enter name"
+            icon={icons.person}
+            value={form.name}
+            onChangeText={(value) => setForm({ ...form, name: value })}
+          />
+          <InputField
+            label="Email"
+            placeholder="Enter email"
+            icon={icons.email}
+            textContentType="emailAddress"
+            value={form.email}
+            onChangeText={(value) => setForm({ ...form, email: value })}
+          />
+          <InputField
+            label="Password"
+            placeholder="Enter password"
+            icon={icons.lock}
+            secureTextEntry={true}
+            textContentType="password"
+            value={form.password}
+            onChangeText={(value) => setForm({ ...form, password: value })}
+          />
 
-    const [form, setForm] = useState ({
-        name: "",
-        email: "",
-        password: "",
-     });
-    return(
-        <ScrollView className="flex-1 bg-white">
-            <View className="flex-1 bg-white">
-                <View className="relative w-full h-[250px]">
-                    <Image source={images.signUpCar}  className="z-0 w-full h-[250px]"/>
-                    <Text className="text-2xl text-black font-JakartaSemiBold absolute bottom-5 left-5">
-                        Create Your Account
-                    </Text>
-                </View>
-                <View className="p-5">
-                    <InputField 
-                        label="Name" 
-                        placeholder="Enter your name" 
-                        icon={icons.person}
-                        value={form.name}
-                        onChangeText={(value) => setForm({...form, name: value})}
-                    />
-                    <InputField
-                        label="Email"
-                        placeholder="Enter email"
-                        icon={icons.email}
-                        textContentType="emailAddress"
-                        value={form.email}
-                        onChangeText={(value) => setForm({ ...form, email: value })}
-                    />
-                    <InputField
-                        label="Password"
-                        placeholder="Enter password"
-                        icon={icons.lock}
-                        secureTextEntry={true}
-                        textContentType="password"
-                        value={form.password}
-                        onChangeText={(value) => setForm({ ...form, password: value })}
-                    />
-
-                    <CustomButton title="Signu Up" onPress={onSignUpPress} className="mt-6" />
-
-                    <OAuth />
-
-                    <Link
-                        href="/sign-up"
-                        className="text-lg text-center text-general-200 mt-10"
-                    >
-                        Already have an account?{" "}
-                        <Text className="text-primary-500">Log In</Text>
-                    </Link>
-                </View>
-            </View>
-        </ScrollView>
-        )
-    }
-
+          <OAuth />
+          <Link
+            href="/sign-in"
+            className="text-lg text-center text-general-200 mt-10"
+          >
+            Already have an account?{" "}
+            <Text className="text-primary-500">Log In</Text>
+          </Link>
+        </View>
+          <View className="bg-white px-7 py-9 rounded-2xl min-h-[300px]">
+            <Text className="font-JakartaExtraBold text-2xl mb-2">
+              Verification
+            </Text>
+            <Text className="font-Jakarta mb-5">
+              We've sent a verification code to {form.email}.
+            </Text>
+            <InputField
+              label={"Code"}
+              icon={icons.lock}
+              placeholder={"12345"}
+              value={verification.code}
+              keyboardType="numeric"
+              onChangeText={(code) =>
+                setVerification({ ...verification, code })
+              }
+            />
+            {verification.error && (
+              <Text className="text-red-500 text-sm mt-1">
+                {verification.error}
+              </Text>
+            )}
+      </View>
+      </View>
+    </ScrollView>
+  );
+};
 export default SignUp;
