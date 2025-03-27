a2c277dd273639f68cf2edf7e9f67e0d70bd5a57
AvdylDemukajj <avdyldemukaj38@gmail.com>
2025-03-27 11:09:42 +0100

    "home age"

--- app/(root)/(tabs)/home.tsx
+++ app/(root)/(tabs)/home.tsx
@@ -1,12 +1,25 @@
-import { Text, View } from "react-native"
-import { SafeAreaView } from "react-native-safe-area-context";
+import { SignedIn, SignedOut, useUser } from '@clerk/clerk-expo'
+import { Link } from 'expo-router'
+import { Text, View } from 'react-native'
+import { SignOutButton } from '@/components/SignOutButton'
 
- const Home = () => {
-    return(
-        <SafeAreaView>
-            <Text>Home</Text>
-        </SafeAreaView>
-        )
-    }
+export default function Page() {
+  const { user } = useUser()
 
-export default Home;
+  return (
+    <View>
+      <SignedIn>
+        <Text>Hello {user?.emailAddresses[0].emailAddress}</Text>
+        <SignOutButton />
+      </SignedIn>
+      <SignedOut>
+        <Link href="/(auth)/sign-in">
+          <Text>Sign in</Text>
+        </Link>
+        <Link href="/(auth)/sign-up">
+          <Text>Sign up</Text>
+        </Link>
+      </SignedOut>
+    </View>
+  )
+}
